12059eac8da350a3b3056cacd5f3f69b58f2e6f3
RyutoKitagawa <ryu51920@gmail.com>
2019-11-03 15:37:53 -1000

    First Run

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/stuff/Contacts.js';
 
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
 function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+  console.log(`  Adding: ${data.firstName} (${data.owner})`);
+  Contacts.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+if (Contacts.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+    Meteor.settings.defaultContacts.map(data => addData(data));
   }
 }
